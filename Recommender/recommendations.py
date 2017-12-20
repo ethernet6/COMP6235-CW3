e0ebc3df8f5c6e8c10835adc325e9f6a82bac29f
jrleader <lixiaoming1993@gmail.com>
2017-12-21 00:09:45 +0800

    Re-uploaded the correct file

--- Recommender/recommendations.py
+++ Recommender/recommendations.py
@@ -1,8 +1,8 @@
 from math import sqrt
 import operator
+
 # A dictionary of movie critics and their ratings of a small
 # set of movies
-
 critics = {
   'Lisa Rose': {
     'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
@@ -38,52 +38,43 @@ critics = {
 
 # Returns a distance based similarity score for person1 and person2
 def sim_distance (prefs, p1, p2):
+    # Create a dict that holds the scores
     shared_items = {}
-    # Loop through collections of preferred items for one person; if the current
-    # item is also in the collection of the other person then it is deemed as
-    # a shared item
+
+    # Get the list of mutually rated items
     for item in prefs[p1]:
         if item in prefs[p2]:
             shared_items[item] = 1
+
+    # No common items found, so return 0
     if(len(shared_items) == 0): return 0
+
+    # Calculate the score
     sum_of_squares = sum([pow(prefs[p1][item] - prefs[p2][item], 2)
     for item in shared_items])
     return 1 / (1 + sqrt(sum_of_squares))
 
-# Find out which two persons have the most similar taste
+# Find out which two persons have the most similar taste, with respect to the Euclidean distance
 def find_most_similar_pair(prefs):
-    # Create an empty dictionary that holds the pairs of people and their
-    # similarity scores
     scores = {}
-    # Get the names of people
     people = list(prefs.keys())
-    # Compute the scores
     for i in range(0, len(people)):
         for j in range(i + 1, len(people)):
-            # Code...
             sim_score = sim_distance(prefs, people[i], people[j])
             scores[(people[i], people[j])] = sim_score
-    # for item in scores.values():
-    # print(scores)
-    # operator.itemgetter(i) sorts a list of tuples by the i-th item
-    # sorted_scores = sorted(scores.items(), key=operator.itemgetter(1),
-    # Sort the entries in the dictionary by score descending
-    sorted_scores = sorted(scores.items(), key=lambda tuple: tuple[1], reverse=True) # Python 3 no longer supports tuple
-                                                                                     # unpacking: 'lambda (x, y): y' causes
-                                                                                     # syntax error
-    # print(sorted_scores)
+    sorted_scores = sorted(scores.items(), key=lambda tuple: tuple[1], reverse=True)
     result = sorted_scores[0]
     print(result)
-    # print("The most similar pair of people is: %s and %s, their similarity score is %.3f"
-    # % result[0][0], result[0][1], result[1])
     print("The most similar pair in terms of Euclidean distance is: " +
           "%s and %s, similarity score %.3f" % (result[0][0], result[0][1], result[1]))
     return result
 
-# Returns the Pearson correlation coefficient for p1 and p2
+# Returns the Pearson correlation coefficient for preferences of p1 and p2
 def sim_pearson(prefs, p1, p2):
-    # Get the list of mutually rated items
+    # Create a list that stores mutually rated items
     shared_items = {}
+
+    # Get the list of mutually rated items
     for item in prefs[p1]:
         if item in prefs[p2]:
             shared_items[item] = 1
@@ -114,23 +105,33 @@ def sim_pearson(prefs, p1, p2):
 
     return score
 
-# Find out which two persons have the most similar taste
+# Find out which two persons have the most similar taste, with respect to Pearson's r
 def find_most_similar_pair_pearson(prefs):
+    # Create a dict that holds the scores
     r_scores = {}
+
+    # Get the list of people
     people = list(prefs.keys())
+
+    # Calculate the score
     for i in range(0, len(people)):
         for j in range(i + 1, len(people)):
             r = sim_pearson(prefs, people[i], people[j])
             pair = (people[i], people[j])
             r_scores[pair] = r
-    # print(r_scores)
+
+    # Sort the scores by value ascending
     r_scores = sorted(r_scores.items(), key=lambda tuple: tuple[1])
-    result = r_scores.pop() # pop() returns the last item in the list by default
-    # print(result)
+
+    # Get the top score
+    result = r_scores.pop()
+
+    # Print the result
     print("The most similar pair in terms of Pearson score " +
           "is: %s and %s, similarity score %.3f" % (result[0][0], result[0][1], result[1]))
     return result
 
+# Print the scores computed by the method given
 def printScore(prefs, p1, p2, scoreType=type):
     if scoreType == 'p': # Pearson
         score = sim_pearson(prefs, p1, p2)
